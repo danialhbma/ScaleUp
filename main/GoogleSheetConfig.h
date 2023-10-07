3463d32ff365f13f8de97f018ae86d0377104b8a
Danial Hazeem <danialhbma@gmail.com>
2023-10-07 15:27:20 +0800

    Added codes for MQTT & Google Sheets config files

--- main/GoogleSheetConfig.h
+++ main/GoogleSheetConfig.h
@@ -1,1 +1,19 @@
 // paste the contents from GoogleSheetConfig.txt here
+// google sheet configuration file
+// defines the google sheet to pull data from
+
+#ifndef GOOGLE_SHEET_CONFIG_H
+#define GOOGLE_SHEET_CONFIG_H
+
+String GOOGLE_SHEET_BASE_API = "https://sheets.googleapis.com/v4/spreadsheets/";
+String GOOGLE_SHEET_ID = "";
+String GOOGLE_SHEET_API_ENDPOINT = GOOGLE_SHEET_BASE_API + GOOGLE_SHEET_ID;
+String GOOGLE_SHEET_API_KEY = ""; 
+
+// range of cells to get data from (A1:Z gets all cells)
+String GOOGLE_SHEET_CELL_RANGE = "A1:Z"; 
+
+// api endpoint to request google sheet data
+String GOOGLE_SHEET_REQUEST_URL = GOOGLE_SHEET_API_ENDPOINT + "/values/" + GOOGLE_SHEET_CELL_RANGE + "?key=" + GOOGLE_SHEET_API_KEY;
+
+#endif
--- main/MQTTConfig.h
+++ main/MQTTConfig.h
@@ -1,1 +1,18 @@
 // paste the MQTTConfig.txt file contents here
+#ifndef MQTT_CONFIG_H
+#define MQTT_CONFIG_H
+
+const char* MQTTServer = "";
+const int MQTTPort = 8883; // Replace with your MQTT broker's port
+const char* MQTTUsername = "";
+const char* MQTTPassword = "";
+const char* MQTTClientID = "ESP32_Client";
+const char* MQTTTopic = ""; // Replace with the MQTT topic you want to publish/subscribe to
+
+const char* MQTTCACert =R"EOF(
+  -----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)EOF";
+
+
+#endif
